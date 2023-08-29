918a1ffe98c946726969450b03a0ca1bb0190747
reeeken <109532456+reeeken@users.noreply.github.com>
2023-08-29 17:04:14 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import {} from "antd";
-import { BrowserRouter } from "react-router-dom";
-import "./App.css";
-import AppHeader from "./components/AppHeader";
-import AppFooter from "./components/AppFooter";
-import PageContent from "./components/PageContent";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+import AppHeader from './components/AppHeader';
+import AppFooter from './components/AppFooter';
+import PageContent from './components/PageContent';
+import SellingOverview from './components/SellingOverview';
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <AppHeader />
-        <PageContent />
-        <AppFooter />
-      </BrowserRouter>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <AppHeader />
+                <Routes>
+                    <Route path="/" element={<PageContent />} />
+                    <Route path="/selling-overview" element={<SellingOverview />} />
+                    {/* You can add more routes as your application grows */}
+                </Routes>
+                <AppFooter />
+            </Router>
+        </div>
+    );
 }
 
 export default App;
